1d38faba8d5269d8ac827fbe87a32eb1a3806b51
urixofficial <urixofficial@gmail.com>
2025-05-30 16:43:59 +1000

    Добавлен дебаунсинг для слайдеров. Исправлено масштабирование.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainApplication:
         set_logger()
         numeric_log_level = logger.level(log_level).no if isinstance(log_level, str) else log_level
         self.logger = logger.bind(module_level=numeric_log_level)
-        logger.debug("Инициализация приложения")
+        logger.info("Инициализация приложения")
 
         self.app = QApplication(sys.argv)
 
@@ -29,11 +29,34 @@ class MainApplication:
         self.animation_timer.timeout.connect(self.update_animation)
         self.is_animating = False
 
+        # Словарь для таймеров дебаунсинга
+        self.debounce_timers = {}
+        self.debounce_delay = 50  # Задержка дебаунсинга в миллисекундах
+
         self.set_signals()
+
+    def create_debounce_timer(self, callback):
+        """Создаёт и возвращает таймер для дебаунсинга с указанным callback."""
+        timer = QTimer()
+        timer.setSingleShot(True)
+        timer.timeout.connect(callback)
+        return timer
+
+    def debounce(self, slider, callback):
+        """Функция дебаунсинга для слайдера."""
+
+        def handler():
+            if slider in self.debounce_timers:
+                self.debounce_timers[slider].stop()
+            else:
+                self.debounce_timers[slider] = self.create_debounce_timer(callback)
+            self.debounce_timers[slider].start(self.debounce_delay)
+
+        return handler
 
     def set_signals(self):
         """Подключение сигналов"""
-        self.logger.debug("Подключение сигналов")
+        self.logger.info("Подключение сигналов")
         try:
             self.ui.width_input.valueChanged.connect(self.animation_manager.set_width)
             self.ui.width_input.valueChanged.connect(self.ui.update_canvas_size)
@@ -41,42 +64,73 @@ class MainApplication:
             self.ui.height_input.valueChanged.connect(self.ui.update_canvas_size)
             self.ui.fps_input.valueChanged.connect(self.animation_manager.set_fps)
             self.ui.duration_input.valueChanged.connect(self.animation_manager.set_duration)
-            self.ui.points_amount_slider.valueChanged.connect(self.animation_manager.set_points_amount)
+
+            # Подключение сигналов для слайдеров с дебаунсингом
+            self.ui.points_amount_slider.valueChanged.connect(
+                self.debounce(self.ui.points_amount_slider,
+                              lambda: self.animation_manager.set_points_amount(self.ui.points_amount_slider.value())))
             self.ui.points_check.toggled.connect(self.render_manager.set_points_check)
-            self.ui.points_size_slider.valueChanged.connect(self.render_manager.set_points_size)
+            self.ui.points_size_slider.valueChanged.connect(
+                self.debounce(self.ui.points_size_slider,
+                              lambda: self.render_manager.set_points_size(self.ui.points_size_slider.value())))
             self.ui.lines_check.toggled.connect(self.render_manager.set_lines_check)
-            self.ui.lines_width_slider.valueChanged.connect(self.render_manager.set_lines_width)
+            self.ui.lines_width_slider.valueChanged.connect(
+                self.debounce(self.ui.lines_width_slider,
+                              lambda: self.render_manager.set_lines_width(self.ui.lines_width_slider.value())))
             self.ui.fill_check.toggled.connect(self.render_manager.set_fill_check)
-            self.ui.fill_variation_slider.valueChanged.connect(self.render_manager.set_fill_variation)
-            self.ui.hue_slider.valueChanged.connect(self.render_manager.set_hue)
-            self.ui.saturation_slider.valueChanged.connect(self.render_manager.set_saturation)
-            self.ui.brightness_slider.valueChanged.connect(self.render_manager.set_brightness)
-            self.ui.bg_hue_slider.valueChanged.connect(self.render_manager.set_bg_hue)
-            self.ui.bg_saturation_slider.valueChanged.connect(self.render_manager.set_bg_saturation)
-            self.ui.bg_brightness_slider.valueChanged.connect(self.render_manager.set_bg_brightness)
-            self.ui.animation_speed_slider.valueChanged.connect(self.animation_manager.set_animation_speed)
-            self.ui.transition_speed_slider.valueChanged.connect(self.animation_manager.set_transition_speed)
+            self.ui.fill_variation_slider.valueChanged.connect(
+                self.debounce(self.ui.fill_variation_slider,
+                              lambda: self.render_manager.set_fill_variation(self.ui.fill_variation_slider.value())))
+            self.ui.hue_slider.valueChanged.connect(
+                self.debounce(self.ui.hue_slider,
+                              lambda: self.render_manager.set_hue(self.ui.hue_slider.value())))
+            self.ui.saturation_slider.valueChanged.connect(
+                self.debounce(self.ui.saturation_slider,
+                              lambda: self.render_manager.set_saturation(self.ui.saturation_slider.value())))
+            self.ui.brightness_slider.valueChanged.connect(
+                self.debounce(self.ui.brightness_slider,
+                              lambda: self.render_manager.set_brightness(self.ui.brightness_slider.value())))
+            self.ui.bg_hue_slider.valueChanged.connect(
+                self.debounce(self.ui.bg_hue_slider,
+                              lambda: self.render_manager.set_bg_hue(self.ui.bg_hue_slider.value())))
+            self.ui.bg_saturation_slider.valueChanged.connect(
+                self.debounce(self.ui.bg_saturation_slider,
+                              lambda: self.render_manager.set_bg_saturation(self.ui.bg_saturation_slider.value())))
+            self.ui.bg_brightness_slider.valueChanged.connect(
+                self.debounce(self.ui.bg_brightness_slider,
+                              lambda: self.render_manager.set_bg_brightness(self.ui.bg_brightness_slider.value())))
+            self.ui.animation_speed_slider.valueChanged.connect(
+                self.debounce(self.ui.animation_speed_slider,
+                              lambda: self.animation_manager.set_animation_speed(
+                                  self.ui.animation_speed_slider.value())))
+            self.ui.transition_speed_slider.valueChanged.connect(
+                self.debounce(self.ui.transition_speed_slider,
+                              lambda: self.animation_manager.set_transition_speed(
+                                  self.ui.transition_speed_slider.value())))
+
+            # Подключение кнопок
             self.ui.generate_frame_btn.clicked.connect(self.generate_frame)
             self.ui.export_frame_btn.clicked.connect(self.export_frame)
             self.ui.start_animation_btn.clicked.connect(self.start_animation)
             self.ui.export_animation_btn.clicked.connect(self.export_animation)
+
         except AttributeError as e:
             logger.error(f"Ошибка при подключении сигналов: {e}")
             raise
 
     def generate_frame(self):
         """Генерация кадра"""
-        self.logger.debug("Запуск генерации кадра")
+        self.logger.info("Запуск генерации кадра")
         self.animation_manager.init_frame()
         frame = self.animation_manager.get_frame()
         self.render_manager.render_frame(frame)
 
     def export_frame(self):
-        self.logger.debug("Экспорт кадра")
-        self.ui.canvas.save_image()
+        self.logger.info("Экспорт кадра")
+        self.render_manager.save_image()
 
     def start_animation(self):
-        self.logger.debug("Запуск/остановка анимации")
+        self.logger.info("Запуск/остановка анимации")
         if not self.is_animating:
             # Запускаем анимацию
             fps = self.ui.fps_input.value()
@@ -86,12 +140,12 @@ class MainApplication:
             interval = 1000 // fps  # Интервал в миллисекундах
             self.animation_timer.start(interval)
             self.is_animating = True
-            self.ui.start_animation_btn.setText("Остановить анимацию")
+            self.ui.start_animation_btn.setText("Остановка анимации")
         else:
             # Останавливаем анимацию
             self.animation_timer.stop()
             self.is_animating = False
-            self.ui.start_animation_btn.setText("Запустить анимацию")
+            self.ui.start_animation_btn.setText("Запуск анимации")
 
     def update_animation(self):
         self.logger.debug("Обновление анимации")
@@ -109,5 +163,5 @@ class MainApplication:
         sys.exit(self.app.exec())
 
 if __name__ == '__main__':
-    app = MainApplication("DEBUG")
+    app = MainApplication("INFO")
     app.run()
--- modules/render_manager.py
+++ modules/render_manager.py
@@ -1,192 +1,195 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout
-from pyqtgraph import PlotWidget, mkPen, mkBrush
+import pygame
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtCore import Qt, QSize
 from modules.utils import hsv_to_rgb
 from loguru import logger
 
 
-class RenderManager():
-	def __init__(self, config_manager, canvas, log_level="INFO"):
-		super().__init__()
-		numeric_log_level = logger.level(log_level).no if isinstance(log_level, str) else log_level
-		self.logger = logger.bind(module_level=numeric_log_level)
-		self.logger.debug("Инициализация холста")
+class RenderManager:
+    def __init__(self, config_manager, canvas, log_level="INFO"):
+        numeric_log_level = logger.level(log_level).no if isinstance(log_level, str) else log_level
+        self.logger = logger.bind(module_level=numeric_log_level)
+        self.logger.debug("Инициализация холста")
 
-		self.config = config_manager
+        self.config = config_manager
+        self.canvas = canvas
 
-		# Инициализация параметров из конфига
-		self.points_check = self.config.get_bool("GenerationParams", "points_check")
-		self.points_size = self.config.get_int("GenerationParams", "points_size_default")
-		self.lines_check = self.config.get_bool("GenerationParams", "lines_check")
-		self.lines_width = self.config.get_int("GenerationParams", "lines_width_default")
-		self.fill_check = self.config.get_bool("GenerationParams", "fill_check")
-		self.fill_variation = self.config.get_int("GenerationParams", "fill_variation_default")
-		self.hsv_color = {
-			"h": self.config.get_int("ColorParams", "hue_default"),
-			"s": self.config.get_int("ColorParams", "saturation_default"),
-			"v": self.config.get_int("ColorParams", "brightness_default")
-		}
-		self.hsv_bg_color = {
-			"h": self.config.get_int("ColorParams", "bg_hue_default"),
-			"s": self.config.get_int("ColorParams", "bg_saturation_default"),
-			"v": self.config.get_int("ColorParams", "bg_brightness_default")
-		}
-		self.rgb_color = hsv_to_rgb(
-			self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"]
-		)
-		self.rgb_bg_color = hsv_to_rgb(
-			self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"]
-		)
-		self.frame_width = self.config.get_int("ImageParams", "width_default")
-		self.frame_height = self.config.get_int("ImageParams", "height_default")
-		self.frame = None
+        # Инициализация параметров из конфига
+        self.points_check = self.config.get_bool("GenerationParams", "points_check")
+        self.points_size = self.config.get_int("GenerationParams", "points_size_default")
+        self.lines_check = self.config.get_bool("GenerationParams", "lines_check")
+        self.lines_width = self.config.get_int("GenerationParams", "lines_width_default")
+        self.fill_check = self.config.get_bool("GenerationParams", "fill_check")
+        self.fill_variation = self.config.get_int("GenerationParams", "fill_variation_default")
+        self.hsv_color = {
+            "h": self.config.get_int("ColorParams", "hue_default"),
+            "s": self.config.get_int("ColorParams", "saturation_default"),
+            "v": self.config.get_int("ColorParams", "brightness_default")
+        }
+        self.hsv_bg_color = {
+            "h": self.config.get_int("ColorParams", "bg_hue_default"),
+            "s": self.config.get_int("ColorParams", "bg_saturation_default"),
+            "v": self.config.get_int("ColorParams", "bg_brightness_default")
+        }
+        self.rgb_color = hsv_to_rgb(
+            self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"]
+        )
+        self.rgb_bg_color = hsv_to_rgb(
+            self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"]
+        )
+        self.frame_width = self.config.get_int("ImageParams", "width_default")
+        self.frame_height = self.config.get_int("ImageParams", "height_default")
+        self.frame = None
 
-		self.canvas = canvas
-		self.plot_widget = PlotWidget()
-		self.plot_widget.setBackground(self.rgb_bg_color)
+        # Инициализация Pygame
+        pygame.init()
+        self.screen = pygame.Surface((self.frame_width, self.frame_height))
+        self.screen.fill(self.rgb_bg_color)
 
-		# Настройка области отображения
-		self.plot_item = self.plot_widget.getPlotItem()
-		self.plot_item.setAspectLocked(True)
-		self.plot_item.setRange(xRange=(0, self.frame_width), yRange=(0, self.frame_height))
-		self.plot_item.invertY(False)
+        # Настройка PySide6 для отображения Pygame Surface
+        self.canvas_widget = QLabel()
+        self.canvas_layout = QVBoxLayout()
+        self.canvas_layout.addWidget(self.canvas_widget)
+        self.canvas.setLayout(self.canvas_layout)
 
-		# Отключаем оси
-		self.plot_item.hideAxis('left')  # Скрываем ось Y
-		self.plot_item.hideAxis('bottom')  # Скрываем ось X
+    def render_frame(self, frame):
+        """Отрисовка кадра."""
+        self.logger.debug("Отрисовка кадра")
+        if not frame:
+            self.logger.warning("Получен пустой кадр")
+            return
+        self.frame = frame
 
-		# Отключаем масштабирование и перемещение мышью
-		self.plot_item.setMouseEnabled(x=False, y=False)
+        # Очистка поверхности
+        self.screen.fill(self.rgb_bg_color)
 
-		# Отключаем автоматическое изменение диапазона
-		self.plot_item.enableAutoRange(x=False, y=False)
+        # Отрисовка элементов
+        if self.points_check and "points" in self.frame:
+            self.draw_points(self.frame["points"])
+        if self.lines_check and "lines" in self.frame:
+            self.draw_lines(self.frame["lines"])
+        if self.fill_check and "fill" in self.frame:
+            self.draw_fill(self.frame["fill"])
 
-		# Добавляем виджет в layout
-		layout = QVBoxLayout()
-		layout.addWidget(self.plot_widget)
-		self.canvas.setLayout(layout)
+        # Преобразование Pygame Surface в QImage для отображения в PySide6
+        pygame_image = pygame.image.tostring(self.screen, "RGB")
+        qimage = QImage(pygame_image, self.frame_width, self.frame_height, QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimage)
+        self.canvas_widget.setPixmap(pixmap.scaled(self.canvas_widget.size(), Qt.AspectRatioMode.KeepAspectRatio))
 
-	def render_frame(self, frame):
-		"""Отрисовка кадра."""
-		self.logger.debug("Отрисовка кадра")
-		if not frame:
-			self.logger.warning("Получен пустой кадр")
-			return
-		self.frame = frame
-		self.plot_item.clear()  # Очищаем предыдущий контент
-		if self.points_check and "points" in self.frame:
-			self.draw_points(self.frame["points"])
-		if self.lines_check and "lines" in self.frame:
-			self.draw_lines(self.frame["lines"])
-		if self.fill_check and "fill" in self.frame:
-			self.draw_fill(self.frame["fill"])
+    def draw_points(self, points):
+        """Отрисовка точек."""
+        self.logger.debug(f"Отрисовка {len(points)} точек с размером {self.points_size}")
+        for point in points:
+            try:
+                x, y = int(point[0]), int(point[1])
+                pygame.draw.circle(self.screen, self.rgb_color, (x, y), self.points_size // 2)
+            except (IndexError, TypeError) as e:
+                self.logger.error(f"Ошибка при отрисовке точки {point}: {e}")
 
-	def draw_points(self, points):
-		self.logger.debug(f"Отрисовка {len(points)} точек с размером {self.points_size}")
-		x = points[:, 0]
-		y = points[:, 1]
-		pen = mkPen(color=self.rgb_color, width=0)  # Белый цвет для теста
-		brush = mkBrush(color=self.rgb_color)
-		self.plot_item.plot(x, y, pen=None, symbol='o', symbolSize=self.points_size, symbolPen=pen, symbolBrush=brush)
+    def draw_lines(self, lines):
+        """Отрисовка линий."""
+        self.logger.debug(f"Отрисовка {len(lines)} линий с толщиной {self.lines_width}")
+        if not lines:
+            self.logger.debug("Нет линий для отрисовки")
+            return
+        for line in lines:
+            try:
+                start_pos = (int(line[0]), int(line[1]))
+                end_pos = (int(line[2]), int(line[3]))
+                pygame.draw.line(self.screen, self.rgb_color, start_pos, end_pos, self.lines_width)
+            except (IndexError, TypeError) as e:
+                self.logger.error(f"Ошибка при отрисовке линии {line}: {e}")
 
-	def draw_lines(self, lines):
-		self.logger.debug(f"Отрисовка {len(lines)} линий с толщиной {self.lines_width}")
-		if not lines:
-			self.logger.debug("Нет линий для отрисовки")
-			return
-		pen = mkPen(color=self.rgb_color, width=self.lines_width)
-		for line in lines:
-			try:
-				x = [line[0], line[2]]  # x-координаты начала и конца линии
-				y = [line[1], line[3]]  # y-координаты начала и конца линии
-				self.plot_item.plot(x, y, pen=pen, symbol=None)
-			except (IndexError, TypeError) as e:
-				self.logger.error(f"Ошибка при отрисовке линии {line}: {e}")
+    def draw_fill(self, lines):
+        """Отрисовка заливки (заглушка, так как не реализована в оригинале)."""
+        self.logger.debug("Отрисовка заливки")
+        # TODO: Реализовать заливку, если нужно (например, с использованием pygame.draw.polygon)
 
-	def draw_fill(self, lines):
-		self.logger.debug("Отрисовка заливки")
+    def save_image(self):
+        """Сохранение изображения."""
+        self.logger.debug("Сохранение изображения")
+        pygame.image.save(self.screen, "frame.png")
 
-	def save_image(self):
-		"""Сохранение изображения."""
-		self.logger.debug("Сохранение изображения")
+    def set_points_check(self, flag):
+        self.logger.debug(f"Установка флага отображения точек {flag}")
+        self.points_check = flag
+        self.render_frame(self.frame)
 
-	def set_points_check(self, flag):
-		self.logger.debug(f"Установка флага отображения точек {flag}")
-		self.points_check = flag
-		self.render_frame(self.frame)
+    def set_points_size(self, value):
+        self.logger.debug(f"Установка размера точек: {value}")
+        self.points_size = value
+        self.render_frame(self.frame)
 
-	def set_points_size(self, value):
-		self.logger.debug(f"Установка размера точек: {value}")
-		self.points_size = value
-		self.render_frame(self.frame)
+    def set_lines_check(self, flag):
+        self.logger.debug(f"Установка флага отображения линий {flag}")
+        self.lines_check = flag
+        self.render_frame(self.frame)
 
-	def set_lines_check(self, flag):
-		self.logger.debug(f"Установка флага отображения линий {flag}")
-		self.lines_check = flag
-		self.render_frame(self.frame)
+    def set_lines_width(self, value):
+        self.logger.debug(f"Установка ширины линий: {value}")
+        self.lines_width = value
+        self.render_frame(self.frame)
 
-	def set_lines_width(self, value):
-		self.logger.debug(f"Установка ширины линий: {value}")
-		self.lines_width = value
-		self.render_frame(self.frame)
+    def set_fill_check(self, flag):
+        self.logger.debug(f"Установка флага отображения заливки {flag}")
+        self.fill_check = flag
+        self.render_frame(self.frame)
 
-	def set_fill_check(self, flag):
-		self.logger.debug(f"Установка флага отображения заливки {flag}")
-		self.fill_check = flag
-		self.render_frame(self.frame)
+    def set_fill_variation(self, value):
+        self.logger.debug(f"Установка разброса яркости заливки: {value}")
+        self.fill_variation = value
+        self.render_frame(self.frame)
 
-	def set_fill_variation(self, value):
-		self.logger.debug(f"Установка разброса яркости заливки: {value}")
-		self.fill_variation = value
-		self.render_frame(self.frame)
+    def set_hue(self, value):
+        self.logger.debug(f"Установка оттенка основного цвета: {value}")
+        self.hsv_color["h"] = value
+        self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
+        self.render_frame(self.frame)
 
-	def set_hue(self, value):
-		self.logger.debug(f"Установка оттенка основного цвета: {value}")
-		self.hsv_color["h"] = value
-		self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
-		self.render_frame(self.frame)
+    def set_saturation(self, value):
+        self.logger.debug(f"Установка насыщенности основного цвета: {value}")
+        self.hsv_color["s"] = value
+        self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
+        self.render_frame(self.frame)
 
-	def set_saturation(self, value):
-		self.logger.debug(f"Установка насыщенности основного цвета: {value}")
-		self.hsv_color["s"] = value
-		self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
-		self.render_frame(self.frame)
+    def set_brightness(self, value):
+        self.logger.debug(f"Установка яркости основного цвета: {value}")
+        self.hsv_color["v"] = value
+        self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
+        self.render_frame(self.frame)
 
-	def set_brightness(self, value):
-		self.logger.debug(f"Установка яркости основного цвета: {value}")
-		self.hsv_color["v"] = value
-		self.rgb_color = hsv_to_rgb(self.hsv_color["h"], self.hsv_color["s"], self.hsv_color["v"])
-		self.render_frame(self.frame)
+    def set_bg_hue(self, value):
+        self.logger.debug(f"Установка оттенка цвета фона: {value}")
+        self.hsv_bg_color["h"] = value
+        self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
+        self.render_frame(self.frame)
 
-	def set_bg_hue(self, value):
-		self.logger.debug(f"Установка оттенка цвета фона: {value}")
-		self.hsv_bg_color["h"] = value
-		self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
-		self.plot_widget.setBackground(self.rgb_bg_color)
-		self.render_frame(self.frame)
+    def set_bg_saturation(self, value):
+        self.logger.debug(f"Установка насыщенности цвета фона: {value}")
+        self.hsv_bg_color["s"] = value
+        self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
+        self.render_frame(self.frame)
 
-	def set_bg_saturation(self, value):
-		self.logger.debug(f"Установка насыщенности цвета фона: {value}")
-		self.hsv_bg_color["s"] = value
-		self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
-		self.plot_widget.setBackground(self.rgb_bg_color)
-		self.render_frame(self.frame)
+    def set_bg_brightness(self, value):
+        self.logger.debug(f"Установка яркости цвета фона: {value}")
+        self.hsv_bg_color["v"] = value
+        self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
+        self.render_frame(self.frame)
 
-	def set_bg_brightness(self, value):
-		self.logger.debug(f"Установка яркости цвета фона: {value}")
-		self.hsv_bg_color["v"] = value
-		self.rgb_bg_color = hsv_to_rgb(self.hsv_bg_color["h"], self.hsv_bg_color["s"], self.hsv_bg_color["v"])
-		self.plot_widget.setBackground(self.rgb_bg_color)
-		self.render_frame(self.frame)
+    def set_color(self, h, s, v):
+        self.logger.debug(f"Установка основного цвета: {h=} {s=} {v=}")
+        self.hsv_color = {"h": h, "s": s, "v": v}
+        self.rgb_color = hsv_to_rgb(h, s, v)
+        self.render_frame(self.frame)
 
-	def set_color(self, h, s, v):
-		self.logger.debug(f"Установка основного цвета: {h=} {s=} {v=}")
-		self.hsv_color = {"h": h, "s": s, "v": v}
-		self.rgb_color = hsv_to_rgb(h, s, v)
-		self.render_frame(self.frame)
+    def set_bg_color(self, h, s, v):
+        self.logger.debug(f"Установка цвета фона: {h=} {s=} {v=}")
+        self.hsv_bg_color = {"h": h, "s": s, "v": v}
+        self.rgb_bg_color = hsv_to_rgb(h, s, v)
+        self.render_frame(self.frame)
 
-	def set_bg_color(self, h, s, v):
-		self.logger.debug(f"Установка цвета фона: {h=} {s=} {v=}")
-		self.hsv_bg_color = {"h": h, "s": s, "v": v}
-		self.rgb_bg_color = hsv_to_rgb(h, s, v)
-		self.plot_widget.setBackground(self.rgb_bg_color)
-		self.render_frame(self.frame)
+    def __del__(self):
+        """Очистка Pygame при уничтожении объекта."""
+        pygame.quit()
