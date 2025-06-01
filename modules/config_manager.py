71d9d33a6c84cd5925e4c3bbf90d5c14badc1b4e
urixofficial <urixofficial@gmail.com>
2025-06-01 14:16:49 +1000

    Добавление пустых областей в конфиг.

--- modules/config_manager.py
+++ modules/config_manager.py
@@ -1,4 +1,5 @@
 import configparser
+import numpy as np
 from loguru import logger
 
 class ConfigManager:
@@ -46,3 +47,19 @@ class ConfigManager:
         except (configparser.NoSectionError, configparser.NoOptionError) as e:
             self.logger.error(f"Ошибка чтения [{section}][{key}]: {e}")
             raise
+
+    def get_empty_areas(self):
+        self.logger.debug("Чтение пустых областей из конфигурации")
+        try:
+            areas = []
+            area_count = self.config.getint("EmptyAreas", "area_count")
+            for i in range(1, area_count + 1):
+                area_key = f"area_{i}"
+                area_str = self.config.get("EmptyAreas", area_key)
+                # Парсим строку в список координат
+                area_points = eval(area_str)  # Безопасность можно улучшить через ast.literal_eval
+                areas.append(np.array(area_points, dtype=np.float64))
+            return areas
+        except (configparser.NoSectionError, configparser.NoOptionError, ValueError) as e:
+            self.logger.error(f"Ошибка чтения пустых областей: {e}")
+            return []
